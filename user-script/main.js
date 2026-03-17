d4a81403bf7d7de0a3c59929356718c0172affa2
Your Name <you@example.com>
2026-03-17 15:04:32 +0600

    OK

--- user-script/main.js
+++ user-script/main.js
@@ -22,6 +22,7 @@
         message_data_unknown: "yellow",
         cloudflare_challenge: "green",
     };
+    const CHAR_LIMIT = 5000;
     const host = document.createElement("div");
     Object.assign(host.style, {
         position: "fixed",
@@ -34,9 +35,6 @@
     document.documentElement.appendChild(host);
     const shadow = host.attachShadow({ mode: "open" });
     const progress = document.createElement("div");
-    const circle = new ProgressBar.Circle(progress, {
-        
-    })
     progress.hidden = true;
     shadow.appendChild(progress);
     const wait_for_element = async (selector) => {
@@ -51,12 +49,12 @@
             })
         );
     };
-    const response_current_url = async (websocket, data) => {
-        if (!("current_url" in data)) return (progress.style.backgroundColor = STATUS_COLORS.message_data_unknown);
+    const translate = async (text) => {}
+    const response_current_job = async (websocket, data) => {
+        if (!("current_index" in data && "text" in data)) return (progress.style.backgroundColor = STATUS_COLORS.message_data_unknown);
         if (document.title == "Just a moment...") return (progress.style.backgroundColor = STATUS_COLORS.cloudflare_challenge);
-        if (data.current_url != window.location.href) return (window.location.href = data.current_url);
-        websocket.send(JSON.stringify({ type: "submit_html", data: { html: document.documentElement.outerHTML } }));
-        websocket.send(JSON.stringify({ type: "request_current_url", data: {} }));
+        websocket.send(JSON.stringify({ type: "submit_text", data: { current_index: data.current_index,text: await translate(data.text) } }));
+        websocket.send(JSON.stringify({ type: "request_current_job", data: {} }));
     };
     const websocket = new WebSocket("ws://127.0.0.1:6969");
     websocket.onopen = () => (progress.hidden = false);
@@ -65,13 +63,13 @@
     websocket.onmessage = async (event) => {
         const message = JSON.parse(event.data);
         const handler_mapping = {
-            response_current_url: response_current_url,
+            response_current_job: response_current_job,
         };
         if (!("type" in message && "data" in message)) return (progress.style.backgroundColor = STATUS_COLORS.message_format_error);
         if (!(message.type in handler_mapping)) return (progress.style.backgroundColor = STATUS_COLORS.message_type_unknown);
         await handler_mapping[message.type](websocket, message.data);
     };
     websocket.addEventListener("open", () => {
-        websocket.send(JSON.stringify({ type: "request_current_url", data: {} }));
+        websocket.send(JSON.stringify({ type: "request_current_job", data: {} }));
     });
 })();
