fedfb8fb2e0dd6446722100febe0f24bdad98f86
Your Name <you@example.com>
2026-03-18 15:24:45 +0600

    OK

--- shell-script/install.sh
+++ shell-script/install.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
-SERVER_URL="https://raw.githubusercontent.com/abdullah-al-jaber/verse-lingua/vanilla/server-script/main.py"
-USER_URL="https://raw.githubusercontent.com/abdullah-al-jaber/verse-lingua/vanilla/user-script/main.js"
-SERVER_PATH="/usr/bin/verse-lingua"
-USER_PATH="/android/verse-lingua.js"
+MAIN_NAME="verse-lingua"
+SERVER_URL="https://raw.githubusercontent.com/abdullah-al-jaber/$MAIN_NAME/vanilla/server-script/main.py"
+USER_URL="https://raw.githubusercontent.com/abdullah-al-jaber/$MAIN_NAME/vanilla/user-script/main.js"
+SERVER_PATH="/usr/bin/$MAIN_NAME"
+USER_PATH="/android/$MAIN_NAME.js"
+MAIN_COMPLETION_PATH="/etc/fish/completions/$MAIN_NAME.fish"
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "[ERROR] Failed to gain root permission !"
-  exit 1
+	echo "Failed to gain root permission !"
+	exit 1
 fi
 
-curl -sSL "$SERVER_URL" -o "$SERVER_PATH" || { echo "Failed to download server script !"; exit 2; }
-chmod +x "$SERVER_PATH" || { echo "Failed to make server script executable !"; exit 3; }
+curl -sSL "$SERVER_URL" -o "$SERVER_PATH" || {
+	echo "Failed to download server script !"
+	exit 2
+}
+chmod +x "$SERVER_PATH" || {
+	echo "Failed to make server script executable !"
+	exit 3
+}
+register-python-argcomplete --shell fish "$MAIN_NAME" >"$MAIN_COMPLETION_PATH" || {
+	echo "Failed to generate FISH completions !"
+	exit 4
+}
 
 echo "Successfully installed server script !"
 
-curl -sSL "$USER_URL" -o "$USER_PATH" || { echo "Failed to download user script !"; exit 4; }
+curl -sSL "$USER_URL" -o "$USER_PATH" || {
+	echo "Failed to download user script !"
+	exit 4
+}
 
 echo "Successfully downloaded user script !"
 echo "Please install user script ! PATH: $USER_PATH !"
--- shell-script/uninstall.sh
+++ shell-script/uninstall.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-SERVER_PATH="/usr/bin/verse-lingua"
-USER_PATH="/android/verse-lingua.js"
+MAIN_NAME="verse-lingua"
+SERVER_PATH="/usr/bin/$MAIN_NAME"
+USER_PATH="/android/$MAIN_NAME.js"
+MAIN_COMPLETION_PATH="/etc/fish/completions/$MAIN_NAME.fish"
 
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Failed to gain root permission !"
-  exit 1
+	echo "Failed to gain root permission !"
+	exit 1
 fi
 
-rm $SERVER_PATH || { echo "Failed to remove server script !"; exit 2; }
+rm $SERVER_PATH || {
+	echo "Failed to remove server script !"
+	exit 2
+}
+rm "$MAIN_COMPLETION_PATH" || {
+	echo "Failed to remove FISH completions !"
+	exit 3
+}
 echo "Successfully uninstalled server script !"
 
-rm -f $USER_PATH || { echo "Failed to remove user script !"; exit 2; }
+rm -f $USER_PATH || {
+	echo "Failed to remove user script !"
+	exit 4
+}
 echo "Successfully removed server script !"
 
 # Final Version
